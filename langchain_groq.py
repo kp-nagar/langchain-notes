3ff3157202bbc0140536f47ce1ee322244dcfde3
Kumarpal Nagar <kumarpal.nagar@gmail.com>
2025-06-17 20:57:51 +0530

    add langchain and groq

---
+++ langchain_groq.py
@@ -0,0 +1,14 @@
+import getpass
+import os
+from langchain.chat_models import init_chat_model
+from langchain.chains.combine_documents import create_stuff_documents_chain
+
+
+# if not os.environ.get("GROQ_API_KEY"):
+#   os.environ["GROQ_API_KEY"] = getpass.getpass("Enter API key for Groq: ")
+
+os.environ["GROQ_API_KEY"] = "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"
+model = init_chat_model("llama3-8b-8192", model_provider="groq")
+
+s = model.invoke("Hello, world!")
+print(s)
