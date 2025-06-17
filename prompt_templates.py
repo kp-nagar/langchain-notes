178d5e161c794295d253b76f7c6d86834208b8fe
Kumarpal Nagar <kumarpal.nagar@gmail.com>
2025-06-17 21:03:28 +0530

    add PT

---
+++ prompt_templates.py
@@ -0,0 +1,27 @@
+from langchain_core.prompts import prompt, ChatPromptTemplate
+import os
+from langchain.chat_models import init_chat_model
+
+prompt_template = prompt.PromptTemplate.from_template(
+    """
+    Tell me a joke about {topic}
+    """
+)
+
+# prompt_template = ChatPromptTemplate([
+#     ("system", "You are a helpful assistant"),
+#     ("user", "Tell me a joke about {topic}")
+# ])
+
+# ask_prompt = prompt_template.invoke({"topic": "cats"})
+# print(ask_prompt)
+
+os.environ["GROQ_API_KEY"] = "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"
+
+model = init_chat_model("llama3-8b-8192", model_provider="groq")
+
+chain = prompt_template | model
+
+res = chain.invoke("cat")
+
+print(res)
